6907dda198ea1a07e9f3bd394029888160468ad1
amar-sinha <amar.sinha@uconn.edu>
2019-07-25 11:14:12 -0400

    reorganized main panel

--- panels.py
+++ panels.py
@@ -2,21 +2,34 @@ import wx, mysql.connector
 
 class mainPanel(wx.Panel):
     # Main Panel will include:
-        # Admin Panel Option
-        # Employee Panel Option
+        # Keypad for PIN entry
+        # On Enter Press: will check with DB and send user to Admin or Employee Panel
+
     def __init__(self, parent):
         wx.Panel.__init__(self, parent=parent)
         title = 'employee-time-tracker'
 
-        self.btnAdminPanel = wx.Button(self, -1, "Admin Login", (360, 290))
-        self.btnUserPanel = wx.Button(self, -1, "Employee Panel", (495, 290))
-
-        wx.StaticText(self, -1, 'Employee Time Tracking', (410, 245))
-
         png = wx.Image('images/logo.png')
         png.Rescale(300, 80)
         logo = wx.Bitmap(png)
-        wx.StaticBitmap(self, -1, logo, (340, 140), style=wx.BITMAP_TYPE_PNG)
+        wx.StaticBitmap(self, -1, logo, (340, 100), style=wx.BITMAP_TYPE_PNG)
+
+        wx.StaticText(self, -1, 'Employee Time Tracking', (410, 205))
+
+        self.pin = wx.TextCtrl(self, pos=(440, 250), size=(90, 24))
+
+        self.btnOne = wx.Button(self, -1, "1", (440, 280), size=(30,30))
+        self.btnTwo = wx.Button(self, -1, "2", (470, 280), size=(30, 30))
+        self.btnThree = wx.Button(self, -1, "3", (500, 280), size=(30, 30))        
+        self.btnFour = wx.Button(self, -1, "4", (440, 310), size=(30, 30))
+        self.btnFive = wx.Button(self, -1, "5", (470, 310), size=(30, 30))
+        self.btnSix = wx.Button(self, -1, "6", (500, 310), size=(30, 30))
+        self.btnSeven = wx.Button(self, -1, "7", (440, 340), size=(30, 30))
+        self.btnEight = wx.Button(self, -1, "8", (470, 340), size=(30, 30))
+        self.btnNine = wx.Button(self, -1, "9", (500, 340), size=(30, 30))
+        self.btnZero = wx.Button(self, -1, "0", (470, 370), size=(30, 30))
+
+        self.btnEnter = wx.Button(self, -1, "Enter", (445, 410))
 
 class adminPanel(wx.Panel):
     # Admin Panel
@@ -26,15 +39,8 @@ class adminPanel(wx.Panel):
         wx.Panel.__init__(self, parent=parent)
         self.btn = wx.Button(self, -1, "Back to Main Menu", (350, 150))
 
-class userPanel(wx.Panel):
-    # User Panel
-        # Employee Selection (enter a four digit PIN) -> sends to Time Record Panel
-    def __init__(self, parent):
-        wx.Panel.__init__(self, parent=parent)
-        self.btn = wx.Button(self, -1, "Back to Main Menu", (350, 150))
-
-class recordPanel(wx.Panel):
-    # Record Panel
+class empPanel(wx.Panel):
+    # Employee Panel
         # Start / Stop Time Clock
     def __init__(self, parent):
         wx.Panel.__init__(self, parent=parent)
--- program.py
+++ program.py
@@ -9,19 +9,9 @@ class Program(wx.Frame):
 
         self.panel_one = panels.mainPanel(self)
         self.panel_two = panels.adminPanel(self)
-        self.panel_three = panels.userPanel(self)
+        self.panel_three = panels.empPanel(self)
 
         sizer.Add(self.panel_one, 1, wx.EXPAND)
-
-        self.panel_one.btnAdminPanel.Bind(wx.EVT_BUTTON, self.show_panel_two)
-        sizer.Add(self.panel_two, 1, wx.EXPAND)
-        self.panel_two.btn.Bind(wx.EVT_BUTTON, self.show_panel_one)
-        self.panel_two.Hide()
-
-        self.panel_one.btnUserPanel.Bind(wx.EVT_BUTTON, self.show_panel_three)
-        sizer.Add(self.panel_three, 1, wx.EXPAND)
-        self.panel_three.btn.Bind(wx.EVT_BUTTON, self.show_panel_one)
-        self.panel_three.Hide()
 
         self.SetSize((960, 540))
         self.Centre()
